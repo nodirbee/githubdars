5bdcfe849f3d1a406c9d27c8c32aa37bb12fa9f5
nodirbee <nodirbekigamberdiev005@gmail.com>
2023-12-23 23:16:20 +0300

    boshladim1

---
+++ js.js
@@ -0,0 +1,34 @@
+//____________________________________________ 1-dars___________________________________________________
+
+// let a = 'salom'
+// a = hello;
+// console.log(a)
+
+// const b = {
+// ism:"nodir",
+// nomer:01550129857,
+// yosh:18,
+// yashash_goy:'egypt',
+
+// }
+
+// console.log(b)
+
+
+//__________________________________________2-dars_________________________________________________________
+
+
+let a = 'salom'
+
+switch(a){
+case 'salom2' :
+console.log('men toptim case-1');
+break;
+case 'salom' :
+console.log('men toptim case-2');
+break;
+
+}
+
+
+
